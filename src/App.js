d2567507e1bff5ce846a1295c2a3949971f9b77d
Awlad909 <mdawladbdinfo@gmail.com>
2022-06-04 23:12:02 -0700

    redux toolkit fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CounterView from "./app/features/counter/CounterView";
+import PostView from "./app/features/posts/PostView";
 
 function App() {
   return (
     <div className="App">
       <CounterView />
+      <PostView />
     </div>
   );
 }
